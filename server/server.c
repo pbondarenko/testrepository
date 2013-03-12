f544c724046c16bc9efbdd41f529d28bb5fad9c7
pbondarenko <polinabond@gmail.com>
2013-03-12 15:11:23 +0400

    select

--- server/server.c
+++ server/server.c
@@ -5,142 +5,151 @@
  *      Author: polina
  */
 
-#include <signal.h>
-#include <event.h>
-#include <event2/listener.h>
-#include <event2/buffer.h>
-#include <event2/bufferevent.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <errno.h>
+#include <unistd.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/select.h>
+#include <arpa/inet.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <limits.h>
 
-static void function_write( struct bufferevent *buf_ev, void * arg);
-static void echo_event_cb( struct bufferevent *buf_ev, short events, void *arg );
+#define OPEN_MAX 1024
+int fd[OPEN_MAX];
 
-/* Функция обратного вызова для события: данные готовы для чтения в buf_ev */
-static void function_read( struct bufferevent *buf_ev, void *arg )
-{
+int main() {
 
-	char path[1024];
-	bufferevent_read(buf_ev, path, 1024);
-	printf("Server reader: get file path: %s\n", path);
-	int fd = open(path, O_RDONLY);
+	fd_set master;
 
-	if(fd >= 0) {
-		printf("Server reader: file desc %d\n", fd);
-		bufferevent_enable( buf_ev, EV_WRITE);
-		bufferevent_disable( buf_ev, EV_READ);
-		bufferevent_setcb(buf_ev, NULL, function_write, echo_event_cb, (void *)fd);
+	/* temp file descriptor list for select() */
+	fd_set read_fds;
+	fd_set write_fds;
 
-	} else {
-		arg = NULL;
-		printf("Server reader: file not exist\n");
+	struct sockaddr_in serv_addr, cli_addr;
+	int sockfd, newsockfd, fdmax, i;   /* int fdmax is the maximum file descriptor number */
+	socklen_t clilen;
 
-		char message[1024];
-		strcpy(message,"file not exist");
-		printf("Server reader: message: %s\n", message);
+	char buf[1024];
 
-		bufferevent_write(buf_ev, message, 1024);
-		bufferevent_disable(buf_ev, EV_READ);
-		bufferevent_setcb(buf_ev, NULL, function_write, echo_event_cb, (void*)(-1));
-	}
+	/* clear the master and temp sets */
 
-}
-static void function_write( struct bufferevent *buf_ev, void * arg){
+	FD_ZERO(&master);
+
+	FD_ZERO(&read_fds);
+
+	FD_ZERO(&write_fds);
 
 
-	int fd = (int)arg;
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		perror("socket");
 
-	if(fd >= 0){
-		printf("Server writer: file desc %d\n", fd);
-		char buf[1024];
-		int size;
+	/* bind */
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(1234);
 
-		memset(buf, 0, sizeof buf);
-		size = read(fd, buf, 1024);
-		printf("Server writer: size of buffer %d\n", size);
-		if(size > 0){
-			bufferevent_write(buf_ev, buf, size);
-			bufferevent_setcb(buf_ev, NULL, function_write, echo_event_cb, (void*)fd);
-		}else{
-			close(fd);
-			printf("Server writer: sent file\n");
-			bufferevent_free(buf_ev);
+	if(bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+		perror("bind");
+
+
+	listen(sockfd, 10);
+	clilen = sizeof(cli_addr);
+
+	/* add the sockfd to the master set */
+	FD_SET(sockfd, &master);
+
+	/* keep track of the biggest file descriptor */
+	fdmax = sockfd; /* so far, it's this one*/
+
+
+	/*Main loop */
+	for( ; ; ) {
+
+	/* copy it */
+		read_fds = master;
+		write_fds = master;
+		struct timeval tv;
+		tv.tv_sec = 1;
+		tv.tv_usec = 0;
+
+		if(select(fdmax+1, &read_fds, &write_fds, NULL, NULL) < 0)
+			perror("select");
+
+		/*run through the existing connections looking for data to be read*/
+		if(fdmax >= OPEN_MAX){
+			perror("open_max");
+			return 1;
 		}
-	}else{
-		printf("Server writer: close buffer\n");
-		bufferevent_free(buf_ev);
+		for(i = 0; i <= fdmax; i++) {
+			if(FD_ISSET(i, &read_fds)) {  /* we got one... */
+				printf("Server %d: read\n", i);
+					if(i == sockfd) {
+
+						if((newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen)) < 0)
+							perror("accept");
+
+						FD_SET(newsockfd, &master); /* add to master set */
+						printf("Server %d: accept new client %d\n", i, newsockfd);
+						/* keep track of the maximum */
+						fd[newsockfd] = -1;
+						if(newsockfd > fdmax) {
+							fdmax = newsockfd;
+						}
+					}
+					else {
+						/* handle data from a client */
+						memset(buf, 0, sizeof buf);
+						int size;
+						if((size = recv(i, buf, 1024, 0)) <= 0) {
+							continue;
+						}
+						else {
+							/* we got some data from a client*/
+							printf("Server %d: path %s\n", i, buf);
+							fd[i] = open(buf, O_RDONLY);
+							printf("Server %d: file desc %d\n", i, fd[i]);
+							if(fd[i] < 0){
+								memset(buf, 0, sizeof buf);
+								memcpy(buf,"file not exist", 14);
+								close(fd[i]);
+								if(send(i, buf, 1024, 0) < 0)
+									perror("send");
+								printf("Server %d: %s\n", i, buf);
+								close(i);
+								FD_CLR(i, &master);
+								FD_CLR(i, &read_fds);
+								FD_CLR(i, &write_fds);
+								continue;
+							}
+						}
+					}
+			}
+			if(FD_ISSET(i, &write_fds) && fd[i] >= 0){
+				printf("Server %d: write\n", i);
+
+				memset(buf, 0, sizeof buf);
+				int size = read(fd[i], buf, 1024);
+
+				if(size == 0){
+					close(fd[i]);
+					printf("Server %d: sent file\n", i);
+					close(i);
+					FD_CLR(i, &master);
+					FD_CLR(i, &read_fds);
+					FD_CLR(i, &write_fds);
+					continue;
+				}
+				printf("Server %d: send %d\n", i, size);
+				if(send(i, buf, 1024, 0) < 0)
+					perror("send");
+
+			}
+		}
 	}
-
-}
-static void echo_event_cb( struct bufferevent *buf_ev, short events, void *arg )
-{
-	int fd = (int)arg;
-
-	if( events & BEV_EVENT_ERROR ){
-		perror( "error bufferevent" );
-	}
-  if( events & (BEV_EVENT_EOF|BEV_EVENT_ERROR) ){
-	  if(fd >= 0){
-		  printf("Server: close fd\n");
-	  	  close(fd);
-	  }
-	  bufferevent_free( buf_ev );
-	  printf("Server: im free 2\n");
-  }
-}
-static void accept_error_cb( struct evconnlistener *listener, void *arg )
-{
-  struct event_base *base = evconnlistener_get_base( listener );
-  int error = EVUTIL_SOCKET_ERROR();
-  printf("Server: closed\n");
-  fprintf( stderr, "Ошибка %d (%s) в мониторе соединений. Завершение работы.\n",
-           error, evutil_socket_error_to_string( error ) );
-  event_base_loopexit( base, NULL );
-}
-static void accept_connection_cb( struct evconnlistener *listener,
-                   evutil_socket_t fd, struct sockaddr *addr, int sock_len,
-                   void *arg )
-{
-  /* При обработке запроса нового соединения необходимо создать для него
-     объект bufferevent */
-  struct event_base *base = evconnlistener_get_base(listener);
-  struct bufferevent *buf_ev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
-
-  bufferevent_setcb( buf_ev, function_read, NULL, echo_event_cb, NULL);
-  bufferevent_enable( buf_ev, EV_READ);
-}
-int main(){
-  signal(SIGPIPE, SIG_IGN);
-  struct event_base *base;
-  struct evconnlistener *listener;
-  struct sockaddr_in sin;
-
-  base = event_base_new();
-  if(!base){
-    perror("event base" );
-    return 1;
-  }
-
-  memset(&sin, 0, sizeof(sin));
-  sin.sin_family = AF_INET;
-  sin.sin_addr.s_addr = htonl( INADDR_ANY );
-  sin.sin_port = htons(1234);
-  void * arg = NULL;
-  listener = evconnlistener_new_bind(base, accept_connection_cb, arg,
-                                     (LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE),
-                                     -1, (struct sockaddr *)&sin, sizeof(sin));
-  if(!listener){
-    perror("listener");
-    return -1;
-  }
-
-  event_base_dispatch(base);
   return 0;
 }
